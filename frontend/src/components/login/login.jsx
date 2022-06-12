5214631eb418a4fa68ab065d67f2d4b9ef508963
unknown <encynot@gmail.com>
2022-06-12 17:17:18 +0700

    progress login

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import { Button, Container, Grid, Paper, TextField } from "@material-ui/core";
 import "./login.css";
 
-function login() {
-  return (
-    <div className="main-login">
-      <div className="login-contain"></div>
-      Log in To Your
-    </div>
-  );
+export default class Login extends React.Component {
+  state = {
+    email: "",
+    password: "",
+  };
+  render() {
+    const { email, password } = this.state;
+    return (
+      <container>
+        <Grid Container style={{ justifyContent: "center" }}>
+          <Grid xs="4">
+            <form>
+              <TextField variant="outlined" size="small" value={email} label="email" required />
+              <TextField variant="outlined" size="small" value={password} label="Password" required />
+              <Button variant="contained" color="primary">
+                Login
+              </Button>
+            </form>
+          </Grid>
+        </Grid>
+      </container>
+    );
+  }
 }
-export default login;
