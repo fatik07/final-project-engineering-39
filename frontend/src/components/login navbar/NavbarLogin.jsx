af4454d25fd9706252375122af8778b79828033e
unknown <encynot@gmail.com>
2022-06-14 18:17:24 +0700

    login

--- frontend/src/components/login navbar/NavbarLogin.jsx
+++ frontend/src/components/login navbar/NavbarLogin.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./navbarlogin.css";
+import "./Navbar.css";
 import img from "../../assets/img/img-login.png";
 
 let sectionStyle = {
@@ -19,26 +19,34 @@ function navbar() {
         <div className="flex-row align-items-end">
           <div className="container">
             <div className="row justify-content-end">
-              <div className="col-md-5 login-col-custom ms-auto">
-                <div className="card p-4 cek">
+              <div className="col-md-6 login-col-custom ms-5 me-5">
+                <div className="card p-3 cek">
                   <div className="card-body">
-                    <div className="form-group">
-                      <h2> LOG IN TO YOUR ACCOUNT</h2>
-                      <label>Email</label>
-                      <input type="text" placeholder="Email" className="form-control" />
+                    <div className="form-group row mb-2">
+                      <h2 className="text-center">Login To Your Account</h2>
+
+                      <label for="inputEmail3" class="col-sm-3 col-form-label">
+                        Email
+                      </label>
+                      <div class="col-sm-10"></div>
+                      <input type="text" id="inputEmail3" placeholder="Email" className="form-control" />
                     </div>
 
-                    <div className="form-group">
-                      <label>Password</label>
+                    <div className="form-group row mb-3">
+                      <label for="inputPassword3" className="col-sm-3 col-form-label">
+                        Password
+                      </label>
+                      <div class="col-sm-10"></div>
                       <input type="password" placeholder="Password" className="form-control" />
                     </div>
-                    <div className="buttonLogin">
+
+                    <div className="buttonLogin row mb-3">
                       <button type="button" class="btn btn-success">
                         Login
                       </button>
                     </div>
                     <p className="text-black">
-                      dont have an account? <a href="register"> Register </a>{" "}
+                      dont have an account? <a href="register"> Register </a>
                     </p>
                   </div>
                 </div>
