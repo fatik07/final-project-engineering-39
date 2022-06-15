c84411ef194e8f15f668b9f5a1f532fc5600c5b7
unknown <encynot@gmail.com>
2022-06-15 17:11:14 +0700

    update login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
+// import logo from './logo.svg';
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Register from "./pages/Register";
 // import { BrowserRouter as RouterRoutes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 
 function App() {
   return (
