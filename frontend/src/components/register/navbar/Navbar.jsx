fd5eb6270e07f4e49d3886d7837c133393196f18
Akhnerf24 <74636992+Akhnerf24@users.noreply.github.com>
2022-06-15 22:25:56 +0700

    Update or change email to username
    From backend, adding name input type in register

--- frontend/src/components/register/navbar/Navbar.jsx
+++ frontend/src/components/register/navbar/Navbar.jsx
@@ -19,23 +19,23 @@ export default function Navbar() {
 
                     <form>
                         <div class="form-group row mb-3">
-                            <label for="inputUsername3" class="col-sm-2 col-form-label">Username</label>
+                            <label for="inputName" class="col-sm-2 col-form-label">Full Name</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" id="inputUsername3" placeholder="Username"></input>
+                                <input type="text" class="form-control" id="inputName" placeholder="name"></input>
                             </div>
                         </div>
 
                         <div class="form-group row mb-3">
-                            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                            <label for="inputUsername" class="col-sm-2 col-form-label">Username</label>
                             <div class="col-sm-10">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email"></input>
+                                <input type="text" class="form-control" id="inputUsername" placeholder="username"></input>
                             </div>
                         </div>
                     
                         <div class="form-group row mb-3">
                             <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
                             <div class="col-sm-10">
-                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password"></input>
+                                <input type="password" class="form-control" id="inputPassword3" placeholder="password"></input>
                             </div>
                         </div>
 
