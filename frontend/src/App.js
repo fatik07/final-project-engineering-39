5dff8fbf6f04d0fcfb1c669d18ab9ffea9a6c771
fatik07 <fatichur.r07@gmail.com>
2022-06-15 09:21:06 +0700

    Chore: update navbar and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,9 +10,9 @@ import Login from "./pages/login";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
