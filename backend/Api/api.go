6f7bbd07c522d7be34b5afd46e3cf59a3ee6b18e
ananda <ananda.sugianto@binus.ac.id>
2022-06-24 21:44:31 +0700

    fix testing and crud

--- backend/Api/api.go
+++ backend/Api/api.go
@@ -27,10 +27,10 @@ func NewAPI(userRepo repository.UserRepo, adminRepo repository.AdminRepo) *API {
 	gin.POST("/Add", api.AuthMiddleWare(api.AdminMiddleware(api.CreateTask)))
 	gin.DELETE("/Delete", api.AuthMiddleWare(api.AdminMiddleware(api.DeleteTask)))
 	gin.GET("/GetPenulis", api.AuthMiddleWare(api.AdminMiddleware(api.Get_Penulis)))
-	gin.GET("/Get", api.GetTask)
-	gin.GET("/GetTaskById", api.GetTaskById)
+	gin.GET("/Get", api.AuthMiddleWare(api.GetTask))
+	gin.GET("/GetTaskById", api.AuthMiddleWare(api.GetTaskById))
 	gin.GET("/MyProfile", api.AuthMiddleWare(api.GetProfile))
-	gin.GET("/Pagination", api.pagination)
+	gin.GET("/Pagination", api.AuthMiddleWare(api.pagination))
 	//User//
 	gin.Any("/Login", api.Login)
 	gin.Any("/Register", api.Register)
